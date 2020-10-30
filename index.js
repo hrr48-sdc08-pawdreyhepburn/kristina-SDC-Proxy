c844ceb4e34302bf5990fa88acedd9c67b5ff923
kcmcminn <kcmcminn@gmail.com>
2020-10-30 16:59:45 -0500

    Updating proxy

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+require('newrelic');
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
 
 const PORT = 3000;
 const HOST = 'localhost';
 
 app.use(express.static(__dirname));
+app.use(cors());
 
 app.get('/info', (req, res, next) => {
   res.send('This is a proxy service which proxies to the 4 services present in the Team 2 FEC project.');
 });
 
-app.get('/*', (req, res) => {
+app.get('/:productid', (req, res) => {
+  console.log('get', req.params.productid)
   res.sendFile(__dirname + '/index.html');
 });
 
